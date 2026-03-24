0586ae5a1d076d9ab88036306f65401096bbd212
Wilson Montero <wilsommonteroen14@gmail.com>
2026-03-24 00:47:56 -0400

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -77,7 +77,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/Frontend-Empleo/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 };
